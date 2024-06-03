23dd5722ca578039a1875de5eba70fd606e2c8e4
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-06-03 19:30:28 +0100

    get all customers

--- src/controllers/customers.controlleer.ts
+++ src/controllers/customers.controlleer.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import prisma from "../utils/prisma";
+import jwt from "jsonwebtoken";
 
 const RegisterNewCustomer = async (req: Request, res: Response) => {
   const { fullName, email, phone, location } = req.body;
@@ -70,4 +71,42 @@ const CreateComplaint = async (req: Request, res: Response) => {
   }
 };
 
-export { RegisterNewCustomer, CreateComplaint };
+const GetAllCustomers = async (req: Request, res: Response) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized: No token provided" });
+  }
+
+  try {
+    const decoded: any = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+
+    if (!decoded) {
+      return res.status(401).json({ message: "Unauthorized: Invalid token" });
+    }
+
+    const customers = await prisma.customer.findMany({
+      include: {
+        complain: true,
+      },
+    });
+
+    return res.status(200).json({
+      message: "customers retrieved successfully",
+      data: customers,
+    });
+  } catch (error) {
+    console.error("Token error:", error);
+
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({ message: "Unauthorized: Token expired" });
+    }
+
+    if (error.name === "JsonWebTokenError") {
+      return res.status(401).json({ message: "Unauthorized: Invalid token" });
+    }
+
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
+export { RegisterNewCustomer, CreateComplaint, GetAllCustomers };
--- src/routes/customers.ts
+++ src/routes/customers.ts
@@ -1,6 +1,7 @@
 import {
   RegisterNewCustomer,
   CreateComplaint,
+  GetAllCustomers,
 } from "../controllers/customers.controlleer";
 import { Router } from "express";
 
@@ -8,5 +9,5 @@ const cusoute = Router();
 
 cusoute.post("/customers/register", RegisterNewCustomer);
 cusoute.post("/customers/complaint", CreateComplaint);
-
+cusoute.get("/customers", GetAllCustomers);
 export { cusoute };
