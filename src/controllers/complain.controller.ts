9c280f8af5e6716591f382073fe2d26b0283736e
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-06-04 03:35:51 +0100

    update

--- src/controllers/complain.controller.ts
+++ src/controllers/complain.controller.ts
@@ -47,4 +47,51 @@ const GetAllComplaints = async (req: Request, res: Response) => {
   }
 };
 
-export { GetAllComplaints };
+const acceptAcomplain = async (req: Request, res: Response) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  const id = req.query.id as string;
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized: No token provided" });
+  }
+  try {
+    const decoded: any = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+    if (!decoded) {
+      return res.status(401).json({ message: "Unauthorized: Invalid token" });
+    }
+    const complaint = await prisma.complain.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    if (!complaint) {
+      return res.status(404).json({ message: "Complaint not found" });
+    }
+    const updatedComplaint = await prisma.complain.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        status: "attending",
+        accountId: decoded.userId,
+      },
+    });
+    return res.status(200).json({
+      message: "Complaint accepted successfully",
+      data: updatedComplaint,
+    });
+  } catch (error) {
+    console.error("Token error:", error);
+
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({ message: "Unauthorized: Token expired" });
+    }
+
+    if (error.name === "JsonWebTokenError") {
+      return res.status(401).json({ message: "Unauthorized: Invalid token" });
+    }
+
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+export { GetAllComplaints, acceptAcomplain };
--- src/routes/complain.ts
+++ src/routes/complain.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { GetAllComplaints } from "../controllers/complain.controller";
+import {
+  GetAllComplaints,
+  acceptAcomplain,
+} from "../controllers/complain.controller";
 
 const complainRoute = Router();
 
 complainRoute.get("/complains", GetAllComplaints);
+complainRoute.get("/complains/accept?:id", acceptAcomplain);
 
 export { complainRoute };
