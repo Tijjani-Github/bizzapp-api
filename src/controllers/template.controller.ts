79f80c0ddf2fd56b89016ba64c9f07e82a9cc098
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-06-04 08:04:08 +0100

    create new template

--- src/controllers/template.controller.ts
+++ src/controllers/template.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import prisma from "../utils/prisma";
+import jwt from "jsonwebtoken";
 
 const GetAllTempleates = async (req: Request, res: Response) => {
   try {
@@ -32,4 +33,44 @@ const getTemplatebyId = async (req: Request, res: Response) => {
   }
 };
 
-export { GetAllTempleates, getTemplatebyId };
+const createTemplate = async (req: Request, res: Response) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  const { answer, question } = req.body;
+
+  if (!answer || !question) {
+    return res.status(400).json({ message: "Please fill all fields" });
+  }
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized: No token provided" });
+  }
+  try {
+    const decoded: any = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+    if (!decoded) {
+      return res.status(401).json({ message: "Unauthorized: Invalid token" });
+    }
+
+    const newTemplate = await prisma.template.create({
+      data: {
+        answer: answer,
+        question: question,
+      },
+    });
+
+    return res.status(201).json({ message: "Template created successfully" });
+  } catch (error) {
+    console.error("Token error:", error);
+
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({ message: "Unauthorized: Token expired" });
+    }
+
+    if (error.name === "JsonWebTokenError") {
+      return res.status(401).json({ message: "Unauthorized: Invalid token" });
+    }
+
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+export { GetAllTempleates, getTemplatebyId, createTemplate };
--- src/routes/templates.ts
+++ src/routes/templates.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   GetAllTempleates,
   getTemplatebyId,
+  createTemplate,
 } from "../controllers/template.controller";
 
 const templateRoute = Router();
 
 templateRoute.get("/templates", GetAllTempleates);
 templateRoute.get("/template?:id", getTemplatebyId);
+templateRoute.post("/template", createTemplate);
 
 export { templateRoute };
