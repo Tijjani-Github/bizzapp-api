091717e5aec9febae90fb7e6b3945dad2a65fad1
James Ayomikun Araoye <ayomikuntemitope.araoye@gmail.com>
2024-06-04 11:18:00 +0100

    Update complain.controller.ts

--- src/controllers/complain.controller.ts
+++ src/controllers/complain.controller.ts
@@ -96,18 +96,13 @@ const acceptAcomplain = async (req: Request, res: Response) => {
 };
 
 const getComplainbyId = async (req: Request, res: Response) => {
-  const token = req.headers.authorization?.split(" ")[1];
+
   const id = req.query.id as string;
 
-  if (!token) {
-    return res.status(401).json({ message: "Unauthorized: No token provided" });
-  }
+  
 
   try {
-    const decoded: any = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
-    if (!decoded) {
-      return res.status(401).json({ message: "Unauthorized: Invalid token" });
-    }
+   
     const complaint = await prisma.complain.findUnique({
       where: {
         id: Number(id),
